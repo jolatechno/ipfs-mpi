dae0f594352af1bfe7f1299b7f3bb1828a5e530d
joseph touzet <joseph.touzet@gmail.com>
2020-03-12 15:56:50 +0100

    Update example.py

--- example_interpretor/example.py
+++ example_interpretor/example.py
@@ -1,6 +1,14 @@
-package example
+import sys
 
-func install(){
-	// install dependency etc..
-	return err
-}
+def install():
+//install package
+
+def run(args):
+//print(result)
+
+if __name__ == "__main__":
+if sys.argv[0] == "install":
+install() 
+
+if sys.argv[0] == "run":
+run(sys.argv[1:])
