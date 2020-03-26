388626d3e5a61b58a3809dcca5ca6f3f8531fdb5
jolatechno <joseph.touzet@gmail.com>
2020-03-26 14:07:30 +0100

    update

---
+++ core/api/utils.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+  "fmt"
+  "strings"
+)
+
+func ListToString(host string, peers []string) string{
+  return fmt.Sprintf("%s,%s", host, strings.Join(peers, ","))
+}
+
+func ListFromString(str string) (string, []string){
+  splited := strings.Split(str, ",")
+  return splited[0], splited[1:]
+}
