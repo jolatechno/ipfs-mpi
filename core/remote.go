d1bb6f15b2ce4718d1b2010106e362dfac44db9e
jolatechno <joseph.touzet@gmail.com>
2020-04-22 16:48:46 +0200

    update

--- core/remote.go
+++ core/remote.go
@@ -267,12 +267,17 @@ func (r *BasicRemote)Reset(stream io.ReadWriteCloser, msgs ...string) {
     }
   }
 
-  scan := bufio.NewScanner(stream)
+  stream.(network.Stream).SetReadDeadline(time.Now().Add(r.PingTimeout))
 
   go r.raiseCheck(send(stream, ResetHandShakeHeader), stream)
-  if scan.Text() != ResetHandShakeHeader {
-    panic(HeaderNotUnderstood)
+  str, err := bufio.NewReader(stream).ReadString('\n')
+  if str != ResetHandShakeHeader + "\n" && err == nil {
+    err = HeaderNotUnderstood
   }
+  if err != nil {
+    panic(err)
+  }
+
 
   received := ResetReader(r.Received, *r.Sent, func(msg string) {
     if err := send(stream, MessageHeader, msg); err != nil {
